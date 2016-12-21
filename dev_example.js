87d66dececae28666257bc5d7c0400c92c5a965e
Ignacio Losiggio <iglosiggio@gmail.com>
2016-12-21 20:12:40 -0300

    Update dev_example.js

--- dev_example.js
+++ dev_example.js
@@ -1,5 +1,5 @@
 /* This isn't a good example, i'm using it for testing the lib for my app */
-require('./index').
-  createHTTPStreamingServer()
+require('./index')
+  .createHTTPStreamingServer()
     .on('request', ({url}) => console.log(url))
     .listen(8080);
